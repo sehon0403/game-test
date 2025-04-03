a1990ca7946e6105433b8e75f2615dfb0b55a7dd
casualfishface <jovannibair@outlook.com>
2025-04-03 14:01:36 +0200

    Ready for programming

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,5 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, Ziggame!", .{});
+    std.debug.print("Hello, Ziggame!\n", .{});
 }
